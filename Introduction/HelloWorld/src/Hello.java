9f2cbf782672a554a26d32bc63218bf273730595
AndrewMarksArt <andrew.colin.marks@gmail.com>
2020-10-01 09:07:56 -0700

    basic folder set up, created new branch for lessons and tutorials that will be merged with master once a section is complete. simple hello world and notes about the main method.

---
+++ Introduction/HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+// Basic hello world program with a description of what the main method is.
+
+public class Hello {
+
+    // The main method is a special method the Java looks for when running
+    // a program it is the entry point of any Java code.
+    public static void main(String[] args) {
+
+        // this is a statement -> a command to call a method println which
+        // prints out "Hello World" to the console/terminal
+        System.out.println("Hello World");
+    }
+}
