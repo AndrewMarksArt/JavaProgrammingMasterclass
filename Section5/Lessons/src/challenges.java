685f84dbb5570fd6938040bf2a8dda7a69b215ce
AndrewMarksArt <andrew.colin.marks@gmail.com>
2020-10-21 11:06:53 -0700

    finihsed switch case day of week challenge.

--- Section5/Lessons/src/challenges.java
+++ Section5/Lessons/src/challenges.java
@@ -1,5 +1,34 @@
 public class challenges {
     public static void main(String[] args) {
-        
+        dayOfWeek(3);
+    }
+
+    public static void dayOfWeek(int day) {
+        switch (day) {
+            case 0:
+                System.out.println("Today is Sunday");
+                break;
+            case 1:
+                System.out.println("Today is Monday");
+                break;
+            case 2:
+                System.out.println("Today is Tuesday");
+                break;
+            case 3:
+                System.out.println("Today is Wednesday");
+                break;
+            case 4:
+                System.out.println("Today is Thursday");
+                break;
+            case 5:
+                System.out.println("Today is Friday");
+                break;
+            case 6:
+                System.out.println("Today is Saturday");
+                break;
+            default:
+                System.out.println("Please try again with a vaild number 0-6");
+                break;
+        }
     }
 }
